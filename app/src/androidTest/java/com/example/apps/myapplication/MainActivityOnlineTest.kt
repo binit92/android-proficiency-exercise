7b5f75b4c2d6cde08b7d7fb378be620f2dfd02a3
binit92 <kumar.binit1992@gmail.com>
2020-07-12 02:11:18 +0530

    Change: Adding two test for Online (MainActivityOnlineTest) and Offline(MainActivityOfflineTest) using Edspresso.
    Signed-off-by: binit92 <kumar.binit1992@gmail.com>

---
+++ app/src/androidTest/java/com/example/apps/myapplication/MainActivityOnlineTest.kt
@@ -0,0 +1,82 @@
+package com.example.apps.myapplication
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onData
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.anything
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class MainActivityOnlineTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun mainActivityOnlineTest() {
+        val linearLayout = onData(anything())
+            .inAdapterView(
+                allOf(
+                    withId(R.id.list_view),
+                    childAtPosition(
+                        withId(R.id.pullToRefresh),
+                        0
+                    )
+                )
+            )
+            .atPosition(2)
+        linearLayout.perform(click())
+
+        val textView = onView(
+            allOf(
+                withText("About Canada"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.action_bar),
+                        childAtPosition(
+                            withId(R.id.action_bar_container),
+                            0
+                        )
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textView.check(matches(withText("About Canada")))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
